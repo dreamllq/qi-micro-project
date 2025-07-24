48cde3e22700a3d6f371dcc60236edc94f003d7e
lvliqi <lvliqi_d@163.com>
2025-07-24 20:04:15 +0800

    feat: up

--- gen-version.js
+++ gen-version.js
@@ -5,19 +5,19 @@ const containerVersionFilePath = path.join(__dirname, 'container-version.json');
 const appsVersionFilePath = path.join(__dirname, 'app-versions.json');
 
 const containerPkgPath = path.join(__dirname, 'public', 'package.json');
-const containerPkg = JSON.parse(fs.readFileSync(containerPkgPath, 'utf8'))
+const containerPkg = JSON.parse(fs.readFileSync(containerPkgPath, 'utf8'));
 
-fs.writeFileSync(containerVersionFilePath, JSON.stringify(containerPkg.version))
+fs.writeFileSync(containerVersionFilePath, JSON.stringify(containerPkg.version));
 
 const appsPath = path.join(__dirname, 'public', 'app');
 
-const apps = {}
-fs.readdirSync(appsPath).forEach(appName=>{ 
+const apps = {};
+fs.readdirSync(appsPath).forEach(appName => { 
   const appPath = path.join(appsPath, appName);
   const appPkgPath = path.join(appPath, 'package.json');
-  const appPkg = JSON.parse(fs.readFileSync(appPkgPath, 'utf8'))
+  const appPkg = JSON.parse(fs.readFileSync(appPkgPath, 'utf8'));
   apps[appName] = appPkg.version;
-})
+});
 
-fs.writeFileSync(appsVersionFilePath, JSON.stringify(apps))
+fs.writeFileSync(appsVersionFilePath, JSON.stringify(apps));
 
--- packages/app/src/pages/demo/index.vue
+++ packages/app/src/pages/demo/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    demo
-  </div>
+  <div id="app" :a="1" class="a" :s="2">dome</div>
 </template>
 
 <script setup lang="ts">
-
+const a = 1;
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.a {
+  color: red;
+  font-size: 20px;
+  background-attachment: fixed;
+}
 </style>
 
 <route>
